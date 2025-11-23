49368d25545e76b27fbcc41588b1164c0d9ce453
Rajveer Singh <therajveersingh@icloud.com>
2025-11-23 20:12:12 +0530

    init

---
+++ init.lua
@@ -0,0 +1,11 @@
+-- Set <space> as the leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = false
+
+require('config.options')
+require('config.keymaps')
+require('config.autocmds')
+require('plugins')
