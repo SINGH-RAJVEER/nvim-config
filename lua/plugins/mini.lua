5cea4246d687dc36367498a0632708cf63d350d8
Rajveer Singh <therajveersingh@icloud.com>
2026-02-03 16:18:34 +0530

    fixed the cursor animation

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,55 +1,56 @@
 return {
-  -- Collection of various small independent plugins/modules
-  {
-    'echasnovski/mini.nvim',
-    config = function()
-      require('mini.ai').setup { n_lines = 500 }
-      require('mini.surround').setup()
-      require('mini.cursorword').setup()
-      require('mini.icons').setup {
-        style = 'glyph',
-        lsp = {
-          Array = { glyph = '' },
-          Boolean = { glyph = '' },
-          Class = { glyph = '' },
-          Color = { glyph = '' },
-          Constant = { glyph = '' },
-          Constructor = { glyph = '' },
-          Enum = { glyph = '' },
-          EnumMember = { glyph = '' },
-          Event = { glyph = '' },
-          Field = { glyph = '' },
-          File = { glyph = '' },
-          Folder = { glyph = '' },
-          Function = { glyph = '' },
-          Interface = { glyph = '' },
-          Keyword = { glyph = '' },
-          Method = { glyph = '' },
-          Module = { glyph = '' },
-          Namespace = { glyph = '' },
-          Null = { glyph = '' },
-          Number = { glyph = '' },
-          Object = { glyph = '' },
-          Operator = { glyph = '' },
-          Package = { glyph = '' },
-          Property = { glyph = '' },
-          Reference = { glyph = '' },
-          Snippet = { glyph = '' },
-          String = { glyph = '' },
-          Struct = { glyph = '' },
-          Text = { glyph = '' },
-          TypeParameter = { glyph = '' },
-          Unit = { glyph = '' },
-          Value = { glyph = '' },
-          Variable = { glyph = '' },
-        },
-      }
-      require('mini.icons').mock_nvim_web_devicons()
-      local statusline = require 'mini.statusline'
-      statusline.setup { use_icons = vim.g.have_nerd_font }
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
-    end,
-  },
+	-- Collection of various small independent plugins/modules
+	{
+		'echasnovski/mini.nvim',
+		config = function()
+			require('mini.ai').setup { n_lines = 500 }
+			require('mini.animate').setup { cursor = { enable = true } }
+			require('mini.surround').setup()
+			require('mini.cursorword').setup()
+			require('mini.icons').setup {
+				style = 'glyph',
+				lsp = {
+					Array = { glyph = '' },
+					Boolean = { glyph = '' },
+					Class = { glyph = '' },
+					Color = { glyph = '' },
+					Constant = { glyph = '' },
+					Constructor = { glyph = '' },
+					Enum = { glyph = '' },
+					EnumMember = { glyph = '' },
+					Event = { glyph = '' },
+					Field = { glyph = '' },
+					File = { glyph = '' },
+					Folder = { glyph = '' },
+					Function = { glyph = '' },
+					Interface = { glyph = '' },
+					Keyword = { glyph = '' },
+					Method = { glyph = '' },
+					Module = { glyph = '' },
+					Namespace = { glyph = '' },
+					Null = { glyph = '' },
+					Number = { glyph = '' },
+					Object = { glyph = '' },
+					Operator = { glyph = '' },
+					Package = { glyph = '' },
+					Property = { glyph = '' },
+					Reference = { glyph = '' },
+					Snippet = { glyph = '' },
+					String = { glyph = '' },
+					Struct = { glyph = '' },
+					Text = { glyph = '' },
+					TypeParameter = { glyph = '' },
+					Unit = { glyph = '' },
+					Value = { glyph = '' },
+					Variable = { glyph = '' },
+				},
+			}
+			require('mini.icons').mock_nvim_web_devicons()
+			local statusline = require 'mini.statusline'
+			statusline.setup { use_icons = vim.g.have_nerd_font }
+			statusline.section_location = function()
+				return '%2l:%-2v'
+			end
+		end,
+	},
 }
