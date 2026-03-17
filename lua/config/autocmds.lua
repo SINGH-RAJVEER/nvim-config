76f93cc89e83c405877de9b37de0e180ba4199ba
Rajveer Singh <therajveersingh@icloud.com>
2026-03-18 00:33:09 +0530

    vimenter now opens with the filetree

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,12 @@
+vim.api.nvim_create_autocmd('VimEnter', {
+  callback = function(data)
+    local no_name = data.file == '' and vim.bo[data.buf].buftype == ''
+    if no_name then
+      vim.cmd('Neotree toggle')
+    end
+  end,
+})
+
 -- [[ Basic Autocommands ]]
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
